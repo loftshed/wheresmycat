78de6d80764c876a306299415c4aeeb944fdffe3
Pete Löfstedt <peter.lofstedt@onespan.com>
2023-11-01 01:15:44 -0400

    feat: firebase setup for push messaging wip

--- server/src/constants/constants.ts
+++ server/src/constants/constants.ts
@@ -4,3 +4,5 @@ export const FINDMY_FILES = [
   `${os.homedir()}/Library/Caches/com.apple.findmy.fmipcore/Items.data`,
   `${os.homedir()}/Library/Caches/com.apple.findmy.fmipcore/Devices.data`,
 ];
+
+export const SERVICE_ACCOUNT_KEY_PATH: string = '../config/serviceAccountKey.json';
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,29 @@
 import { Elysia } from 'elysia';
-import { FINDMY_FILES } from './constants/constants';
+import { FINDMY_FILES, SERVICE_ACCOUNT_KEY_PATH } from './constants/constants';
 import { updateLog } from './utils/logger';
 import chokidar from 'chokidar';
 import { format } from 'date-fns';
+import admin from 'firebase-admin';
 
 const [itemsPath /*, devicesPath */] = FINDMY_FILES;
 const watcher = chokidar.watch(itemsPath, { persistent: true });
+
+const firebase = admin.initializeApp({
+  credential: admin.credential.cert(require(SERVICE_ACCOUNT_KEY_PATH)),
+});
 
 watcher.on('change', () => {
   const timeStamp = format(new Date(), 'yyyy-MM-dd HH:mm:ss');
   console.log(`[${timeStamp}]: Checked for new data`);
   updateLog().catch((error) => console.error('Error updating log:', error));
+  try {
+    firebase.messaging().send({} as admin.messaging.Message);
+  } catch (error) {
+    console.error('Error sending message:', error);
+  }
 });
 
 const app = new Elysia().get('/', () => 'Hello Elysia').listen(3000);
 
-console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
+console.log(`wheresmycat 🐈‍⬛ is running at ${app.server?.hostname}:${app.server?.port}`);
+// console.log(firebase);
